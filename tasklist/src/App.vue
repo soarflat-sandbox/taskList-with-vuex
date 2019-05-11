dd865fd75be2fa38630f163e9be392a99f7b209d
soarflat <soarflat777@gmail.com>
2019-05-11 18:25:24 +0900

    feat: ラベル機能

--- tasklist/src/App.vue
+++ tasklist/src/App.vue
@@ -9,11 +9,27 @@
           @change="toggleTaskStatus(task)"
         />
         {{ task.name }}
+        -
+        <span v-for="id in task.labelIds" :key="id">
+          {{ getLabelText(id) }}
+        </span>
       </li>
     </ul>
 
     <form @submit.prevent="addTask">
       <input type="text" v-model="newTaskName" placeholder="新しいタスク" />
+    </form>
+
+    <h2>ラベル一覧</h2>
+    <ul>
+      <li v-for="label in labels" :key="label.id">
+        <input type="checkbox" :value="label.id" v-model="newTaskLabelIds" />
+        {{ label.text }}
+      </li>
+    </ul>
+
+    <form @submit.prevent="addLabel">
+      <input type="text" v-model="newLabelText" placeholder="新しいラベル" />
     </form>
   </div>
 </template>
@@ -24,24 +40,56 @@ export default {
   data() {
     return {
       // 入力中の新しいタスク名を一時的に保持する
-      newTaskName: ""
+      newTaskName: "",
+
+      // 新しいタスクに紐づくラベル一覧を一時的に保持する
+      newTaskLabelIds: [],
+
+      // 入力中の新しいラベル名を一時的に保持する
+      newLabelText: ""
     };
   },
   computed: {
     tasks() {
+      // Vuex Storeのstateを利用してcomputedを定義している
+      // gettersを定義するのではなく、コンポーネント内で定義するのが一般的？
       return this.$store.state.tasks;
+    },
+    labels() {
+      return this.$store.state.labels;
     }
   },
   methods: {
     // タスクを追加する
     addTask() {
-      this.$store.commit("addTask", { name: this.newTaskName });
+      this.$store.commit("addTask", {
+        name: this.newTaskName,
+        labelIds: this.newTaskLabelIds
+      });
       this.newTaskName = "";
+      this.newTaskLabelIds = [];
     },
 
     // タスクの完了状態を更新する
     toggleTaskStatus(task) {
       this.$store.commit("toggleTaskStatus", { id: task.id });
+    },
+
+    // ラベルを追加する
+    addLabel() {
+      this.$store.commit("addLabel", {
+        text: this.newLabelText
+      });
+      this.newLabelText = "";
+    },
+
+    // ラベルのIDから、そのラベルのテキストを返す
+    // labelsはVuex Storeに保持されているため、
+    // getLabelTextはVuex Storeに定義することも可能だが、
+    // サンプルではコンポーネント内で定義している。このコンポーネントでしか利用しないから？
+    getLabelText(id) {
+      const label = this.labels.filter(label => label.id === id)[0];
+      return label ? label.text : "";
     }
   }
 };
--- tasklist/src/store.js
+++ tasklist/src/store.js
@@ -6,20 +6,37 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     tasks: [
-      { id: 1, name: "牛乳を買う", done: false },
-      { id: 2, name: "みかんを買う", done: false },
-      { id: 3, name: "本を買う", done: true }
+      { id: 1, name: "牛乳を買う", labelIds: [1, 2], done: false },
+      { id: 2, name: "みかんを買う", labelIds: [1, 2], done: false },
+      { id: 3, name: "本を買う", labelIds: [1, 3], done: true }
+    ],
+    labels: [
+      {
+        id: 1,
+        text: "買い物"
+      },
+      {
+        id: 2,
+        text: "食料"
+      },
+      {
+        id: 3,
+        text: "本"
+      }
     ],
     // 次に追加するタスクのID（今回は決め打ち）
-    nextTaskId: 4
+    nextTaskId: 4,
+    // 次に追加するラベルのID（今回は決め打ち）
+    nextLabelId: 4
   },
   mutations: {
     // タスクを追加する
-    addTask(state, { name }) {
+    addTask(state, { name, labelIds }) {
       // NOTE: プッシュで問題ないのか？
       state.tasks.push({
         id: state.nextTaskId,
         name,
+        labelIds,
         done: false
       });
       // 次に追加されるタスクに付与するIDを更新する
@@ -35,6 +52,16 @@ export default new Vuex.Store({
       filtered.forEach(task => {
         task.done = !task.done;
       });
+    },
+
+    // ラベルを追加する
+    addLabel(state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      });
+      // 次に追加されるラベルに付与するIDを更新する
+      state.nextLabelId++;
     }
   },
   actions: {}
