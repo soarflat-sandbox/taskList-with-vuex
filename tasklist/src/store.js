a36d0ce26baac469f352c30873c72d0abe37e59d
soarflat <soarflat777@gmail.com>
2019-05-11 17:42:07 +0900

    feat: タスク一覧をVuex Storeから取得して表示

--- tasklist/src/store.js
+++ tasklist/src/store.js
@@ -4,7 +4,13 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    tasks: [
+      { id: 1, name: "牛乳を買う", done: false },
+      { id: 2, name: "みかんを買う", done: false },
+      { id: 3, name: "本を買う", done: true }
+    ]
+  },
   mutations: {},
   actions: {}
 });
