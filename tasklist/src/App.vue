cf4b238e0a55783e407a6e9c8fbd3b51dc5f6ec5
soarflat <soarflat777@gmail.com>
2019-05-11 18:03:54 +0900

    feat: タスクの新規作成と完了状態の更新機能

--- tasklist/src/App.vue
+++ tasklist/src/App.vue
@@ -3,19 +3,45 @@
     <h2>タスク一覧</h2>
     <ul>
       <li v-for="task in tasks" :key="task.id">
-        <input type="checkbox" :checked="task.done" />
+        <input
+          type="checkbox"
+          :checked="task.done"
+          @change="toggleTaskStatus(task)"
+        />
         {{ task.name }}
       </li>
     </ul>
+
+    <form @submit.prevent="addTask">
+      <input type="text" v-model="newTaskName" placeholder="新しいタスク" />
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: "app",
+  data() {
+    return {
+      // 入力中の新しいタスク名を一時的に保持する
+      newTaskName: ""
+    };
+  },
   computed: {
     tasks() {
       return this.$store.state.tasks;
+    }
+  },
+  methods: {
+    // タスクを追加する
+    addTask() {
+      this.$store.commit("addTask", { name: this.newTaskName });
+      this.newTaskName = "";
+    },
+
+    // タスクの完了状態を更新する
+    toggleTaskStatus(task) {
+      this.$store.commit("toggleTaskStatus", { id: task.id });
     }
   }
 };
--- tasklist/src/store.js
+++ tasklist/src/store.js
@@ -9,8 +9,33 @@ export default new Vuex.Store({
       { id: 1, name: "牛乳を買う", done: false },
       { id: 2, name: "みかんを買う", done: false },
       { id: 3, name: "本を買う", done: true }
-    ]
+    ],
+    // 次に追加するタスクのID（今回は決め打ち）
+    nextTaskId: 4
   },
-  mutations: {},
+  mutations: {
+    // タスクを追加する
+    addTask(state, { name }) {
+      // NOTE: プッシュで問題ないのか？
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        done: false
+      });
+      // 次に追加されるタスクに付与するIDを更新する
+      state.nextTaskId++;
+    },
+
+    // タスクの完了状態を更新する
+    toggleTaskStatus(state, { id }) {
+      const filtered = state.tasks.filter(task => {
+        return task.id === id;
+      });
+
+      filtered.forEach(task => {
+        task.done = !task.done;
+      });
+    }
+  },
   actions: {}
 });
